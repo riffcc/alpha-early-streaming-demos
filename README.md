03add7d22e6188236d49245456a0b99d74ed984a
Benjamin Arntzen <zorlin@gmail.com>
2021-04-16 15:47:17 +0800

    Massive changeset, thanks to benjy, that fixes many things

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# watch
-Riff.CC Streaming Platform
+# Riff.CC Streaming
+This is a test repository of what is intended to be a full streaming platform on Riff.CC
+
+# Credits
+* mesquka
+* benjy@fenders
--- js/watch.js
+++ js/watch.js
@@ -7,13 +7,13 @@ console.log(streamHash);
 document.body.onload = addElement;
 
 function addElement () {
-let newSource = document.createElement("source");
+  let newSource = document.createElement("source");
 
-newSource.url = 'https://cdn.riff.cc/ipfs/' + streamHash;
-newSource.textContent = "Hello, world!";
+  newSource.src = 'https://cdn.riff.cc/ipfs/' + streamHash;
+  newSource.textContent = "Hello, world!";
 
-const videoContainer = document.querySelector('#streaming_ipfs_size'); // Grab the video element
-videoContainer.appendChild(newSource);
+  var videoContainer = document.querySelector('.streaming_ipfs_size'); // Grab the video element
+  videoContainer.appendChild(newSource);
 
-alert(newSource.url);}
+}
 
